14addc726e5008fb49a6916bb9c3cfd717db55c0
Ridho Putra <ridho@closelycoded.net>
2019-03-06 18:02:52 +0700

    up

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const express = require('express')
+const passport = require('passport')
+const GoogleStrategy = require('passport-google-oauth20').Strategy
+
 const app = express() 
 
-app.get('/', (req, res) => {
-  res.send({ hi: 'buddy' })
-})
+passport.use(new GoogleStrategy())
 
-let PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5000
 app.listen(PORT)
