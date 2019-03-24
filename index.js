75771688d19de2b83c14a2bf62dd0e22f13ef10b
Ridho Putra <ridho@closelycoded.net>
2019-03-24 14:55:53 +0700

    refactoring the server.js

--- index.js
+++ index.js
@@ -1,17 +1,9 @@
 const express = require('express')
-const passport = require('passport')
-const GoogleStrategy = require('passport-google-oauth20').Strategy
+require('./services/passport')
 
-const keys = require('./config/keys')
+const app = express()
 
-const app = express() 
-
-passport.use(new GoogleStrategy({
-  clentID: keys.googleClientID,
-  clientSecret: keys.googleClientSecret,
-  callbackURL: '/auth/google/callback',
-  userProfileURL: 'https://googleapis.com/oauth2/v3/userinfo'
-}))
+require('./routes/authRoutes')(app)
 
 const PORT = process.env.PORT || 5000
 app.listen(PORT)
