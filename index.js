f008338c16cb6d733c16621b224792ac179dbdc8
Ridho Putra <muh.ridhoputra@yahoo.com>
2019-04-09 10:44:35 +0700

    'up'

--- index.js
+++ index.js
@@ -23,10 +23,6 @@ app.use(
 app.use(passport.initialize())
 app.use(passport.session())
 
-app.get('/', (req, res) => res.send(
-  { yell: 'Booyah' }
-))
-
 require('./routes/authRoutes')(app)
 require('./routes/billingRoutes')(app)
 
